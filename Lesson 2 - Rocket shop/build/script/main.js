415a335b90c0b2670900e0896298bc4bc8ee1b77
ingasafranova <sfrnv@yahoo.co.uk>
2024-02-05 19:20:12 +0000

    working on nav toggle

---
+++ Lesson 2 - Rocket shop/build/script/main.js
@@ -0,0 +1,16 @@
+const initApp = ()=>{
+    const hamburgerBtn = document.getElementById('mobile-open-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = ()=>{
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu );
+    mobileMenu.addEventListener('click', toggleMenu )
+}
+
+//call the function on HTML document when DOM content is fully loaded, so buttons are there to listen.
+
+document.addEventListener('DOMContentLoaded', initApp)
--- Lesson 2 - Rocket shop/tailwind.config.js
+++ Lesson 2 - Rocket shop/tailwind.config.js
@@ -1,14 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  //look for css classes in all html and js files.
+  content: ["./build/*.html", "./build/script/*.js"],
   theme: {
     extend: {
       screens: {
-        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
-        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'}
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+      },
+      //to bounce down and back
+      keyframes: {
+        'open-menu':{
+        '0%': {transform: 'scaleY(0)'},
+        '80%': {transform: 'scaleY(1.2)'},
+        '100%': {transform: 'scaleY(1)'}
+      } },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+
+        
       }
     },
   },
   plugins: [],
-}
-
+};
